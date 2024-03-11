404fbacc2cce491c91e0b0f25befa41ded9ca5a9
Julie Poulain <juliempoulain@gmail.com>
2024-03-11 13:10:27 -0700

    added functions for lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(saturdayActivity = "roller-skate") {
+  return `This Saturday, I want to ${saturdayActivity}!`;
+}
+
+const mondayWork = function (mondayActivity = "go to the office") {
+  return `This Monday, I will ${mondayActivity}.`;
+};
+
+function wrapAdjective(flair = "*") {
+  return function (adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
